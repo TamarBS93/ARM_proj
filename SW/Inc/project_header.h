afde1c747de032abdca3eb220fdf24288103ad19
tamar <tamar@DESKTOP-36Q4TJ1.localdomain>
2025-08-04 20:01:13 +0300

    first commit

---
+++ SW/Inc/project_header.h
@@ -0,0 +1,50 @@
+#ifndef PROJECTHEADER_H
+#define PROJECTHEADER_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <arpa/inet.h>
+#include <errno.h>
+#include <stdint.h>
+
+/*
+#define SERVER_IP "127.0.0.1"
+#define SERVER_PORT 8080
+#define CLIENT_IP "192.168.1.168"
+#define CLIENT_PORT 5005
+*/
+#define MAX_BIT_PATTERN_LENGTH 256
+
+typedef enum {
+    TIMER = 1,
+    UART = 2,
+    SPI = 4,
+    I2C = 8,
+    ADC_P = 16
+} Peripheral;
+#pragma pack(1)  // Disable padding
+
+typedef struct test_command_t {
+    uint32_t test_id;                               // 4 bytes: Test-ID
+    Peripheral peripheral;                          // 1 byte: Bitfield for peripherals (Timer=1, UART=2, SPI=4, I2C=8, ADC=16)
+    uint8_t iterations;                             // 1 byte: Number of test iterations
+    uint8_t bit_pattern_length;                     // 1 byte: Length of bit pattern
+    uint8_t bit_pattern[MAX_BIT_PATTERN_LENGTH];    // Variable-size, capped array
+} test_command_t;
+
+typedef enum {
+	TEST_ERR = -1,
+	TEST_PASS = 1,
+	TEST_FAIL = 0xff
+} Result;
+
+typedef struct result_pro_t {
+    uint32_t test_id;                // 4 bytes: Test-ID
+    Result test_result;             // bitfield: 1 – test succeeded, 0xff –test failed
+} result_pro_t;
+#pragma pack()  // Restore default packing
+
+
+#endif
---
+++ SW/Src/uarts.c
@@ -0,0 +1,140 @@
+#include "uarts.h"
+
+#define TIMEOUT 	1000 	// ticks (30  millis).
+
+#define UART_SENDER 		(&huart2)
+#define UART_RECEIVER 		(&huart6)
+
+result_pro_t uart_testing(test_command_t* command){
+
+
+	uint8_t tx_buffer[MAX_BIT_PATTERN_LENGTH] = {0};
+	uint8_t rx_buffer[MAX_BIT_PATTERN_LENGTH] = {0};
+
+	result_pro_t response;
+	HAL_StatusTypeDef status;
+
+	if (command == NULL) {
+        printf("UART_TEST: Received NULL command pointer. Skipping.\n");
+        response.test_result = TEST_ERR;
+        return response;
+	}
+
+	response.test_id = command->test_id;
+
+	// Copy pattern to TX buffer
+    memcpy(tx_buffer, command->bit_pattern, command->bit_pattern_length);
+
+	for(uint8_t i=0 ; i< command->iterations ; i++){
+		memset(rx_buffer, 0, sizeof(rx_buffer)); // Clear receive buffer
+
+	    status = HAL_UART_Receive_DMA(UART_RECEIVER, rx_buffer, sizeof(rx_buffer));
+	    if (status != HAL_OK) {
+	        printf("Failed to start receive DMA: %d, error 0x%"PRIX32"\r\n", status, HAL_UART_GetError(UART_RECEIVER));
+	        response.test_result = TEST_FAIL;
+	        vPortFree(command);
+	        return response;
+	    }
+
+        // --- 1. TRANSMIT a block of data via DMA ---
+		status = HAL_UART_Transmit_DMA(UART_SENDER, tx_buffer, command->bit_pattern_length);
+		if (status != HAL_OK) {
+			printf("Failed to sendIT on UART sender: %d, error 0x%"PRIX32"\r\n", status, HAL_UART_GetError(UART_SENDER));
+	        response.test_result = TEST_FAIL;
+	        return response;
+		}
+
+//        // Wait for the TX DMA transfer to complete (semaphore is given in the TxCpltCallback)
+//        // --- Wait for TX to complete
+//        if (xSemaphoreTake(xUartTxSemaphoreHandle, pdMS_TO_TICKS(2000)) != pdPASS) {
+//             printf("TX DMA timeout\n");
+//             response.test_result = TEST_FAIL;
+//             vPortFree(command);
+//             return response;
+//        }
+//        if (xSemaphoreTake(xUartRxSemaphoreHandle, pdMS_TO_TICKS(2000)) != pdPASS) { // 2 second timeout
+//            printf("RX DMA timeout\n");
+//            response.test_result = TEST_FAIL;
+//            vPortFree(command);
+//            return response;
+////            // --- 2. RECEIVE the block of data via DMA ---
+////    		status = HAL_UART_Receive_DMA(UART_RECEIVER, rx_buffer, command->bit_pattern_length);
+////    		if (status != HAL_OK) {
+////    			printf("Failed to receiveIT on UART receiver: %d, error 0x%"PRIX32"\r\n", status, HAL_UART_GetError(UART_RECEIVER));
+////    	        response.test_result = TEST_FAIL;
+////    	        vPortFree(command);
+////    	        return response;
+////    		}
+//        }
+//        else{
+//			printf("Failed to receive a xUartRxSemaphore Token\n");
+//        }
+
+        // --- 3. COMPARE SENT vs. RECEIVED data ---
+        if (command->bit_pattern_length > 100) {
+        	printf("bit_pattern_length more than 100");
+
+//            // Use CRC comparison for large data
+//            uint32_t sent_crc = calculate_crc32(tx_buffer, command->bit_pattern_length);
+//            uint32_t received_crc = calculate_crc32(rx_buffer, command->bit_pattern_length);
+//            if (sent_crc == received_crc) {
+//                response.test_result = PASS;
+//                return response;
+//            } else {
+//                printf("UART_TEST: CRC mismatch on iteration %u. Sent CRC: 0x%lX, Received CRC: 0x%lX\n",
+//                       i + 1, sent_crc, received_crc);
+//                response.test_result = FAIL;
+//                return response;            }
+        } else {
+            // Use memcmp for smaller data
+        	int comp = memcmp(tx_buffer, rx_buffer, command->bit_pattern_length);
+            if (comp != 0)
+            {
+            	printf("memcmp result: %d\n", comp);
+
+                printf("UART_TEST: Data mismatch on iteration %u.\n", i + 1);
+                // debug print mismatch details for small data
+                printf("Sent: %.*s\n", command->bit_pattern_length, tx_buffer);
+                printf("Recv: %.*s\n", command->bit_pattern_length, rx_buffer);
+
+                response.test_result = TEST_FAIL;
+                vPortFree(command);
+                return response;
+            }
+            else{
+                printf("UART_TEST: Data Match on iteration %u.\n", i + 1);
+            }
+        }
+
+        osDelay(10); // Small delay between iterations to prevent overwhelming the UUT or the system
+	}
+    response.test_result = TEST_PASS;
+    vPortFree(command);
+    return response;
+}
+
+
+void HAL_UART_TxCpltCallback(UART_HandleTypeDef *huart)
+{
+    BaseType_t xHigherPriorityTaskWoken = pdFALSE;
+    printf("TX callback fired\n");
+
+    if (huart->Instance == UART_SENDER->Instance) // Check the instance of your sender UART
+    {
+        xSemaphoreGiveFromISR(xUartTxSemaphoreHandle, &xHigherPriorityTaskWoken);
+    }
+    portYIELD_FROM_ISR(xHigherPriorityTaskWoken);
+
+}
+
+void HAL_UART_RxCpltCallback(UART_HandleTypeDef *huart)
+{
+    BaseType_t xHigherPriorityTaskWoken = pdFALSE;
+    printf("RX callback fired\n");
+
+    if (huart->Instance == UART_RECEIVER->Instance) // Check the instance of your receiver UART
+    {
+        xSemaphoreGiveFromISR(xUartRxSemaphoreHandle, &xHigherPriorityTaskWoken);
+    }
+    portYIELD_FROM_ISR(xHigherPriorityTaskWoken);
+}
